c4258b15a1d2a8747f5f05cd934539d2039f385f
Acer <danielsamu191@gmail.com>
2023-11-22 11:54:15 +0200

    jjjj

--- src/componets/ImageProfile.jsx
+++ src/componets/ImageProfile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Avatar from '@mui/material/Avatar';
-
+const a = 0
 const ImageProfile = ({ imageUrl, alt }) => {
   return <Avatar alt={alt} src={imageUrl} />;
 };
