e62ff5f113f84c7642a44692b337b273dc88388c
Acer <danielsamu191@gmail.com>
2023-11-28 12:35:02 +0200

    move the chats from App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import UserProfile from "./componets/Avatar/UserProfile";
+import UserProfile from "./componets/AvatarProfile";
 import ChatList from './componets/ChatList';
 import Grid from '@mui/material/Grid';
 import ListUsers from './componets/listOfConections';
+import chats from './componets/chatsInformation'; // Update the path accordingly
 
 import io from "socket.io-client";
 const socket = io("http://localhost:3011");
 
-function App() {
+function App2() {
   const [isConnected, setIsConnected] = useState(socket.connected);
   useEffect(() => {
     function onConnect() {
@@ -38,30 +39,15 @@ function App() {
       <UserProfile />
         <p>Connected: {isConnected ? "yes" : "no"}</p>
     </>
+  )
+}
 
 
 
 function App() {
-  const chats = [
-    { id: 1, name: 'danielsamuel', avatar: '/avatars/john.jpg', lastMessage: 'Hello there!' ,state: false},
-    { id: 2, name: 'Daniel Waisman', avatar: '/avatars/jane.jpg', lastMessage: 'How are you?', state: false},
-    { id: 3, name: 'Daniel Waisman', avatar: '/avatars/jane.jpg', lastMessage: 'How are you?', state: false},
-    { id: 4, name: 'Daniel Waisman', avatar: '/avatars/jane.jpg', lastMessage: 'How are you?', state: false},
-    { id: 5, name: 'Daniel Waisman', avatar: '/avatars/jane.jpg', lastMessage: 'How are you?', state: false},
-    { id: 6, name: 'Daniel Waisman', avatar: '/avatars/jane.jpg', lastMessage: 'How are you?', state: false},
-    { id: 7, name: 'Daniel Waisman', avatar: '/avatars/jane.jpg', lastMessage: 'How are you?', state: false},
-    { id: 8, name: 'Daniel Waisman', avatar: '/avatars/jane.jpg', lastMessage: 'How are you?', state: false},
-    { id: 9, name: 'Daniel Waisman', avatar: '/avatars/jane.jpg', lastMessage: 'How are you?', state: false},
-    { id: 10, name: 'Daniel Waisman', avatar: '/avatars/jane.jpg', lastMessage: 'How are you?', state: false},
-    { id: 11, name: 'Daniel Waisman', avatar: '/avatars/jane.jpg', lastMessage: 'How are you?', state: false},
-    { id: 12, name: 'Daniel Waisman', avatar: '/avatars/jane.jpg', lastMessage: 'How are you?', state: false},
-    { id: 13, name: 'Daniel Waisman', avatar: '/avatars/jane.jpg', lastMessage: 'How are you?', state: false},
-    { id: 14, name: 'Daniel Waisman', avatar: '/avatars/jane.jpg', lastMessage: 'How are you?', state: false},
-    { id: 15, name: 'Daniel Waisman', avatar: '/avatars/jane.jpg', lastMessage: 'How are you?', state: false},
-    { id: 16, name: 'Daniel Waisman', avatar: '/avatars/jane.jpg', lastMessage: 'How are you?', state: false},
-    
+  
     // Add more chat items as needed
-  ];
+  
 
   return (
     <div   >
@@ -72,4 +58,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
---
+++ src/componets/chatsInformation.jsx
@@ -0,0 +1,9 @@
+// chatsData.js
+const chats = [
+    { id: 1, name: 'danielsamuel', avatar: '/avatars/john.jpg', lastMessage: 'Hello there!', state: false },
+    { id: 2, name: 'Daniel Waisman', avatar: '/avatars/jane.jpg', lastMessage: 'How are you?', state: false },
+    // Add more chat items as needed
+  ];
+  
+  export default chats;
+  
