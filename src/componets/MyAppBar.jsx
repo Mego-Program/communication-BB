f449426424382a498934a34d0051227ef29ccfe4
danielsam191 <58845634+danielsam191@users.noreply.github.com>
2023-12-25 16:47:47 +0200

    Update MyAppBar.jsx

--- src/componets/MyAppBar.jsx
+++ src/componets/MyAppBar.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Avatar } from '@mui/material';
 
-const MyAppBar = () => {
+function MyAppBar({ contactName }) {
+  const firstLetter = contactName ? contactName.charAt(0) : '';
+
   return (
     <AppBar position="fixed" sx={{ width: '83%', backgroundColor: '#F6C927', marginLeft: 'auto' }}>
-    <Toolbar sx={{ display: 'flex', alignItems: 'center' }}>
-      {/* Your User Avatar */}
-      <Avatar alt="User Avatar" src="/static/images/avatar.jpg" sx={{ marginRight: 2 }} />
-  
-      {/* Your Name */}
-      <Typography variant="h6" component="div" sx={{ marginLeft: 'auto', marginRight: 'auto' }}>
-        Contact Name
-      </Typography>
-    </Toolbar>
-  </AppBar>
-  
-  
-
+      <Toolbar sx={{ display: 'flex', alignItems: 'center' }}>
+        {/* Contact's Avatar */}
+        <Avatar alt={`Contact Avatar - ${firstLetter}`} sx={{ marginRight: 2 }}>
+          {firstLetter}
+        </Avatar>
+        {/* Contact's Name */}
+        <Typography variant="h6" component="div" sx={{ marginLeft: 'auto', marginRight: 'auto' }}>
+          {contactName}
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
-};
+}
 
-export default MyAppBar;
+export default MyAppBar;
