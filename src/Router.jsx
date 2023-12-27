0acb39cbbb327b009026d9e71f874bfea2c9837c
mendi <mendiby@gmail.com>
2023-12-27 15:22:01 +0200

    update routing

---
+++ src/Router.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import App from './App.jsx'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import "./App.css"
+import ChatList from './ChatList.jsx'
+
+const router = createBrowserRouter([{
+path: '/messages',
+element: <App/>,
+children: [
+  {
+
+  },
+  
+  {
+    path: 'ChatList/',
+    element: <ChatList/>,
+  }
+]
+}])
+
+const RouterMain = () => {
+    return(
+        <RouterProvider router ={router}/>
+    )
+}
+export default RouterMain
