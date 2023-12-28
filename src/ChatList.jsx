8b2b8b843ec1e2346b236348849dc6b548afa337
Yosi Miller <yosimiller@Yosis-MacBook-Pro.local>
2023-12-28 19:34:36 +0200

    update

--- src/ChatList.jsx
+++ src/ChatList.jsx
@@ -1,88 +1,53 @@
-import{ useEffect, useState } from "react";
-
+import { useEffect, useState } from "react";
 import TextInput from "./componets/TextInput";
 import ButtonSend from "./componets/ButtunSend";
 import ChatEntries from "./componets/ChatEntries";
 import axios from "axios";
-import {  useParams } from "react-router-dom";
-
+import { useParams } from "react-router-dom";
 import { io } from "socket.io-client";
 import { infraApi, api } from "./App";
 
+const socket = io.connect(api);
 
-function ChatList({ id }) {
+function ChatList() {
   const userId = useParams();
   const [newMessage, setNewMessage] = useState("");
   const [chatHistory, setChatHistory] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
   const [usersList, setUserList] = useState([]);
-  const [user, setUser] = useState('');
-  const [historyChat, setHistoryChat] = useState(null);
-
+  const [user, setUser] = useState("");
   useEffect(() => {
     const fetchData = async () => {
       try {
         const token = localStorage.getItem("authToken");
+        console.log(token);
 
-        // Fetch user list
-        const userListResponse = await axios.get(
-          `${infraApi}/api/users/list`,
-          {
-            headers: {
-              authorization: token,
-            },
-          }
-        );
-        setUserList(userListResponse.data.result);
-
-        // Fetch current user
-        const userResponse = await axios.get(
-          `${infraApi}/api/users/me`,
-          {
-            headers: {
-              authorization: token,
-            }, 
-          }
-        );
-        setUser(userResponse.data.result[0]);
+        const response = await axios.get(`${infraApi}/api/users/list`, {
+          headers: {
+            authorization: token,
+          },
+        });
+        const user = await axios.get(`${infraApi}/api/users/me`, {
+          headers: {
+            authorization: token,
+          },
+        });
+        setUserList(response.data.result);
+        console.log("user: ", user.data.result[0]);
+        setUser(user.data.result[0]);
         setIsLoaded(true);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
-
     fetchData();
   }, []);
-
   useEffect(() => {
-    const newTimestamp = new Date();
-    const formattedTimestamp = newTimestamp.toLocaleString();
-
-    // Fetch chat history data
-    const fetchChatHistory = async () => {
-      try {
-        const response = await axios.get(
-          `http://localhost:5001/chat/${user._id}/${userId}`
-        );
-        setHistoryChat(response.data);
-
-        // Include the fetched historyChat in setChatHistory
-        setChatHistory(response.data);
-      } catch (error) {
-        console.error("Error fetching chat history:", error);
-      }
-    };
-
-    fetchChatHistory(); // Fetch initial chat history
-
-    // Listen for socket messages
-    const handleSocketMessage = (message) => {
+    const newTimestamp = new Date(); // Get the current timestamp
+    const formattedTimestamp = newTimestamp.toLocaleString(); // Format the timestamp as a string
+    // get message from server
+    socket.on("message", (message) => {
       console.log(message);
-      
-      // Fetch updated chat history
-      fetchChatHistory();
-
-      // Update chat history with new message
       setChatHistory((prevChatHistory) => [
         ...prevChatHistory,
         {
@@ -90,42 +55,35 @@ function ChatList({ id }) {
           message: message,
           user: {
             id: 2,
-            name: user.firstName + " " + user.lastName,
+            name: user.firstName + " " + user.lastName, //here i want to put the name of the token
             avatar: user.lastName,
           },
         },
       ]);
-    };
-
-    socket.on("message", handleSocketMessage);
-
+    });
     return () => {
-      socket.off("message", handleSocketMessage);
+      socket.off("message");
     };
-  }, [user._id, userId, usersList]);
-  
+  }, [usersList]);
 
-  
   // Function to handle sending a new message
   async function handleSend() {
     if (newMessage.trim() !== "") {
-     console.log(`"sssssssss"${api}/chat/${user._id}/${userId}`)
+      // Perform a POST request to http://localhost:5001
       try {
-
-        const response = await axios.post(`${api}/send/${user._id}/${userId}`, {
+        const urlSent = `${api}/send`;
+        console.log(urlSent);
+        const response = await axios.post(urlSent, {
           text: newMessage,
           local_user: user._id,
-          selectedUserId: userId
+          selectedUserId: userId,
         });
-
         console.log("Message sent to server:", response.data);
       } catch (error) {
         console.error("Error sending message to server:", error);
       }
-
       // Emit the new message to the socket server
       socket.emit("message", newMessage);
-
       // Clear the input for a new message
       setNewMessage("");
     }
@@ -135,26 +93,16 @@ function ChatList({ id }) {
   function handleChange(e) {
     setNewMessage(e.target.value);
   }
-
-
-
   if (!isLoaded) {
     return <p>Loading...</p>;
   }
-
   return (
     <div>
-      
-
       <ChatEntries chatHistory={chatHistory} />
-
       {/* Text input component for entering new messages */}
       <TextInput newMessage={newMessage} handleChange={handleChange} />
-
       {/* Send button component with the SendIcon */}
       <ButtonSend handleSend={handleSend} />
-
-
     </div>
   );
 }
